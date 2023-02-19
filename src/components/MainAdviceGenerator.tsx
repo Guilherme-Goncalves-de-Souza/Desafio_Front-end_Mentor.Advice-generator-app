09e9993faa6ba8fa4cbce854914700994aacbbc2
Guilherme Gonçalves de Souza <gui020fla@outlook.com>
2023-02-18 22:11:22 -0300

    Atualizando projeto

--- src/components/MainAdviceGenerator.tsx
+++ src/components/MainAdviceGenerator.tsx
@@ -14,12 +14,18 @@ const MainAdviceGenerator = () => {
     const [numberAdvice, setNumberAdvice] = useState<number>(117)
     const [advice, setAdvice] = useState<string>("It is easy to sit up and take notice, what's difficult is getting up and taking action.")
 
+    const [error, setError] = useState<boolean>(false)
+
     const getAdvice = async() => {
         setRandomNumber(Math.floor(Math.random() * (200 - 1) * 1))
         const url = `https://api.adviceslip.com/advice/${randomNumber}`
 
-        const res = await axios.get(url)
-        setDados(res.data)
+        try{
+            const res = await axios.get(url)
+            setDados(res.data)
+        } catch(error){
+            setError(true)
+        }
         
         if(dados?.slip.id){
             setNumberAdvice(dados?.slip?.id!)
@@ -42,12 +48,25 @@ const MainAdviceGenerator = () => {
 
     return (
         <main>
-            <p className='adviceId'>Advice #{numberAdvice}</p>
-            <p className='advice'><q>{advice}</q></p>
-            <div className="separator"> </div>
-            <button onClick={handleClickButton}>
-                <img src="/icon-dice.svg" alt="Random Advice" title='Random Advice' />
-            </button>
+
+            {error &&
+                <div className='error'>
+                    <p> :( </p>
+                    <p> Error, try later! </p>
+                </div>
+            }
+
+            {!error &&
+                <>
+                    <p className='adviceId'>Advice #{numberAdvice}</p>
+                    <p className='advice'><q>{advice}</q></p>
+                    <div className="separator"> </div>
+                    <button onClick={handleClickButton}>
+                        <img src="/icon-dice.svg" alt="Random Advice" title='Random Advice' />
+                    </button>
+                </>
+            }
+            
         </main>
     )
 }
